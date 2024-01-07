258f619fec85dc14e3aae9289aacc14074f2b545
Darkroom <56244176+cdf144@users.noreply.github.com>
2024-01-07 18:25:40 +0700

    Binary Search: 'Sqrt(x)'

--- leetcode/binary-search.py
+++ leetcode/binary-search.py
@@ -24,6 +24,19 @@ class Solution:
             return mid + 1
         return mid
 
+    # 69. Sqrt(x)
+    def mySqrt(self, x: int) -> int:
+        low, high = 0, x
+
+        while low < high:
+            mid = low + (high - low) // 2
+            if mid * mid <= x:
+                low = mid + 1
+            else:
+                high = mid
+
+        return low - 1
+
     # 74. Search a 2D Matrix
     def search_matrix(self, matrix: List[List[int]], target: int) -> bool:
         row_num = len(matrix)
