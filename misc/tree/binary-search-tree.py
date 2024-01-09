86a18a5f07e9ed61bebef3f5224ef2904f92666d
Darkroom <56244176+cdf144@users.noreply.github.com>
2024-01-09 10:46:04 +0700

    Add simple BST Data Structure implementation

---
+++ misc/tree/binary-search-tree.py
@@ -0,0 +1,61 @@
+from typing import Optional
+
+
+class _Node:
+    def __init__(self, val: int | str):
+        self.val = val
+        self.left = self.right = None
+
+
+class BST:
+    def __init__(self):
+        self.__root = None
+
+    def insert(self, val: int | str) -> None:
+        self.__root = self.__insert(self.__root, val)
+
+    def __insert(self, x: Optional[_Node], val: int | str) -> _Node:
+        if not x:
+            return _Node(val)
+
+        if val < x.val:
+            x.left = self.__insert(x.left, val)
+        else:
+            x.right = self.__insert(x.right, val)
+
+        return x
+
+    def height(self) -> int:
+        return self.__height(self.__root)
+
+    def __height(self, x: Optional[_Node]) -> int:
+        if not x:
+            return -1
+        return 1 + max(self.__height(x.left), self.__height(x.right))
+
+    def preorder(self) -> None:
+        self.__preorder(self.__root)
+
+    def __preorder(self, x: Optional[_Node]) -> None:
+        if x:
+            print(x.val)
+            self.__preorder(x.left)
+            self.__preorder(x.right)
+
+    def inorder(self) -> None:
+        self.__inorder(self.__root)
+
+    def __inorder(self, x: Optional[_Node]) -> None:
+        if x:
+            self.__inorder(x.left)
+            print(x.val)
+            self.__inorder(x.right)
+
+    def postorder(self) -> None:
+        self.__postorder(self.__root)
+
+    def __postorder(self, x: Optional[_Node]) -> None:
+        if x:
+            self.__postorder(x.left)
+            self.__postorder(x.right)
+            print(x.val)
