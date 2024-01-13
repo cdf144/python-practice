6b57c732f59e2c4bafdc6ed9512cefe2f8faa106
Darkroom <56244176+cdf144@users.noreply.github.com>
2024-01-13 11:46:17 +0700

    Backtracking: 'Subsets' and 'Letter Combinations of a Phone Number'

---
+++ leetcode/backtracking.py
@@ -0,0 +1,50 @@
+from typing import List
+
+
+class Solution:
+    # 17. Letter Combinations of a Phone Number
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        dict_number_letter = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        result = ['']
+        for d in digits:
+            new_result = []
+            for combination in result:
+                for c in dict_number_letter[d]:
+                    new_result.append(combination + c)
+            result = new_result
+
+        return result
+
+    # 78. Subsets
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums_len = len(nums)
+
+        def dfs(start: int, curr_set: List[int]) -> None:
+            """
+            Do a DFS on the problem's decision tree to get all subsets.
+            :param start: The index at which to start considering all the next
+                choices (whether to add a number to the current set or not)
+            :param curr_set: The current set built through making previous
+                decisions
+            """
+            result.append(curr_set)
+
+            for i in range(start, nums_len):
+                dfs(i + 1, curr_set + [nums[i]])
+
+        dfs(0, [])
+        return result
--- leetcode/hash_table.py
+++ leetcode/hash_table.py
@@ -13,32 +13,6 @@ class Solution:
                 return [i, dict_num_to_index[target - num]]
             dict_num_to_index[num] = i
 
-    # 17. Letter Combinations of a Phone Number
-    def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
-
-        dict_number_letter = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz'
-        }
-
-        result = ['']
-        for d in digits:
-            new_result = []
-            for combination in result:
-                for c in dict_number_letter[d]:
-                    new_result.append(combination + c)
-            result = new_result
-
-        return result
-
     # 36. Valid Sudoku
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         seen = set()
