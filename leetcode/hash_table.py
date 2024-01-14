79fdb701e2668fb64698238250b7301fece9ff20
Darkroom <56244176+cdf144@users.noreply.github.com>
2024-01-14 10:00:41 +0700

    Hash Table Daily: 'Determine if Two Strings Are Close'

--- leetcode/hash_table.py
+++ leetcode/hash_table.py
@@ -165,6 +165,18 @@ class Solution:
 
         return max_substr
 
+    # 1657. Determine if Two Strings Are Close
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+
+        count_1 = collections.Counter(word1)
+        count_2 = collections.Counter(word2)
+        return (
+            count_1.keys() == count_2.keys()
+            and sorted(count_1.values()) == sorted(count_2.values())
+        )
+
     # 1897. Redistribute Characters to Make All Strings Equal
     def makeEqual(self, words: List[str]) -> bool:
         length = len(words)
