e3918a0ce618b1dce293eeb6b12464ce32f44328
darkroom <56244176+cdf144@users.noreply.github.com>
2024-02-18 12:20:17 +0700

    Heap Daily: 'Meeting Rooms III'

--- leetcode/heap.py
+++ leetcode/heap.py
@@ -16,3 +16,27 @@ class Solution:
             heapq.heappush(pq, stone_1 - stone_2)
 
         return -pq[0] if pq else 0
+
+    # 2402. Meeting Rooms III
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        meetings.sort()  # key=lambda m: m[0]
+        meetings_held = [0] * n
+
+        available = [i for i in range(n)]
+        using = []  # (end_time, room_no)
+
+        for start, end in meetings:
+            while using and using[0][0] <= start:
+                heapq.heappush(available, heapq.heappop(using)[1])
+
+            room = -1
+            if available:
+                room = heapq.heappop(available)
+                heapq.heappush(using, (end, room))
+            else:
+                delayed_start, room = heapq.heappop(using)
+                duration = end - start
+                heapq.heappush(using, (delayed_start + duration, room))
+            meetings_held[room] += 1
+
+        return meetings_held.index(max(meetings_held))
