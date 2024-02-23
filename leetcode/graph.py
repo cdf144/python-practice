cd3fa1924f517944fc215aa7abb9a65c5c2c01ff
darkroom <56244176+cdf144@users.noreply.github.com>
2024-02-23 18:18:24 +0700

    Graph Daily: 'Cheapest Flights Within K Stops'

--- leetcode/graph.py
+++ leetcode/graph.py
@@ -1,3 +1,5 @@
+import collections
+import math
 from typing import List
 
 
@@ -32,19 +34,6 @@ class UF:
 
 
 class Solution:
-    # 997. Find the Town Judge
-    def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        out_degree = [0] * n
-        in_degree = [0] * n
-        for t in trust:
-            out_degree[t[0] - 1] += 1
-            in_degree[t[1] - 1] += 1
-
-        for i in range(n):
-            if out_degree[i] == 0 and in_degree[i] == n - 1:
-                return i + 1
-        return -1
-
     # 684. Redundant Connection
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         size = 0
@@ -61,3 +50,42 @@ class Solution:
                 result[1] = edge[1]
 
         return result
+
+    # 787. Cheapest Flights Within K Stops
+    def findCheapestPrice(self, n: int, flights: List[List[int]],
+                          src: int, dst: int, k: int) -> int:
+        # BFS, Greedy-ish
+        adj = [[] for _ in range(n)]
+        for u, v, price in flights:
+            adj[u].append((v, price))
+
+        dist_to = [math.inf for _ in range(n)]
+        dist_to[src] = 0
+        queue = collections.deque()
+
+        queue.append((src, 0))
+        stops = 0
+        while queue and stops <= k:
+            for i in range(len(queue)):
+                u, cost = queue.popleft()
+                for v, price in adj[u]:
+                    if dist_to[v] <= cost + price:
+                        continue
+                    dist_to[v] = cost + price
+                    queue.append((v, dist_to[v]))
+            stops += 1
+
+        return dist_to[dst] if dist_to[dst] != math.inf else -1
+
+    # 997. Find the Town Judge
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        out_degree = [0] * n
+        in_degree = [0] * n
+        for t in trust:
+            out_degree[t[0] - 1] += 1
+            in_degree[t[1] - 1] += 1
+
+        for i in range(n):
+            if out_degree[i] == 0 and in_degree[i] == n - 1:
+                return i + 1
+        return -1
