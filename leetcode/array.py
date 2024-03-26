0f3f4101a752943c9a8a366a1971b72a79e6b33b
darkroom <56244176+cdf144@users.noreply.github.com>
2024-03-26 14:14:52 +0700

    Array Daily: 'First Missing Positive'

--- leetcode/array.py
+++ leetcode/array.py
@@ -24,6 +24,20 @@ class Solution:
         # Converting to string
         return str(x)[::-1] == str(x)
 
+    # 41. First Missing Positive
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+        for i, num in enumerate(nums):
+            if num != i + 1:
+                return i + 1
+
+        return n + 1
+
     # 238. Product of Array Except Self
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         # # Extra space
