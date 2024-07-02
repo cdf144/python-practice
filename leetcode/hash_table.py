ddd93fafd7ec4b5c90b2c2c7611e62d4f18049d7
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2024-07-02 10:50:31 +0700

    Hash Table Daily: "Intersection of Two Arrays II"

--- leetcode/hash_table.py
+++ leetcode/hash_table.py
@@ -120,6 +120,18 @@ class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return list(set(nums1) & set(nums2))
 
+    # 350. Intersection of Two Arrays II
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums1) > len(nums2):
+            return self.intersect(nums2, nums1)
+        result = []
+        count = collections.Counter(nums1)
+        for num in nums2:
+            if count[num] > 0:
+                result.append(num)
+                count[num] -= 1
+        return result
+
     # 383. Ransom Note
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         magazine_char_counter = collections.Counter(magazine)
